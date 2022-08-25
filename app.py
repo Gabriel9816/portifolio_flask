39c24d26a0ce9404b545fe6421ca01fc36fd6d71
Gabriel9816 <gabriel.oliveira6@estudante.ifms.edu.br>
2022-08-25 19:14:21 -0400

    dia 25

--- app.py
+++ app.py
@@ -7,11 +7,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
-
-@app.route('/perfil')
-def perfil():
-    return render_template('perfil.html', utc_dt=datetime.datetime.utcnow())
-
-@app.route('/semana3')
-def semana3():
-    return render_template('semana3.html', utc_dt=datetime.datetime.utcnow())
