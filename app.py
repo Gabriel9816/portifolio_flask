fb69e5921e4cd835afc77bdd6d61afd427e051ea
Gabriel9816 <gabriel.oliveira6@estudante.ifms.edu.br>
2022-08-26 19:38:26 -0400

    semana 1 e plano de estudo criado

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import datetime
-from flask import Flask, render_template
+from flask import Flask,  render_template, request
 
 app = Flask(__name__)
 
@@ -7,3 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route("/plano-de-ensino")
+def teaching_plan():
+    return render_template("plan.html")
+
+@app.route("/", methods=["GET", "POST"])
+def simple_calculator():
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        expression = request.form.get('expression')
+        expression = expression.replace('÷','/').replace('𝗑', '*').replace(',', '.')
+        try:
+            result = eval(expression)
+            binnary = bin(int(result)).removeprefix('0b')
+            mensage = None
+        except:
+            mensage = 'Verifique se digitou corretamente'
+            result = binnary = None
+        return render_template("index.html", result=result, binnary=binnary, mensage=mensage)
--- static/js/calculadora.js
+++ static/js/calculadora.js
@@ -1,35 +1,9 @@
-import '/css/calculadora.css';
+function insert(text) {
+  input = document.getElementById('expression');
+  old_value = input.value;
+  input.value = old_value + text;
+}
 
-const calculator = document.querySelector(‘.calculator’)
-const keys = calculator.querySelector(‘.calculator__keys’)
-
-keys.addEventListener(‘click’, e => {
- if (e.target.matches(‘button’)) {
-   // Fazer algo
- }
-})
-if (!action) {
-    if (
-      displayedNum === '0' ||
-      previousKeyType === 'operator' ||
-      previousKeyType === 'calculate'
-    ) {
-      display.textContent = keyContent
-    } else {
-      display.textContent = displayedNum + keyContent
-    }
-    calculator.dataset.previousKeyType = 'number'
-  }
-  
-  if (action === 'decimal') {
-    if (!displayedNum.includes('.')) {
-      display.textContent = displayedNum + '.'
-    } else if (
-      previousKeyType === 'operator' ||
-      previousKeyType === 'calculate'
-    ) {
-      display.textContent = '0.'
-    }
-    
-  calculator.dataset.previousKeyType = 'decimal'
-  }
+function clean() {
+  document.getElementById('expression').value = '';
+}
