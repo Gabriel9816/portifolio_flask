8ae58b1d4159eda8784bdd6c1c228bf2f2942a46
Gabriel9816 <gabriel.oliveira6@estudante.ifms.edu.br>
2022-08-28 02:47:23 -0400

    semana 3 completa

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ from flask import Flask,  render_template, request
 app = Flask(__name__)
 name_list = []
 
+
+def remove_duplicates(lista):
+    empty_list = []
+    for i in lista:
+        if i not in empty_list:
+            empty_list.append(i)
+    empty_list.sort()
+    return empty_list
+
+
+
 @app.route('/', methods=["GET", "POST"])
 def hello():
     teacher = "Alex"
@@ -11,15 +22,31 @@ def hello():
     if request.method == "GET":
         return render_template("index.html", teacher=teacher, students_list=students_list)
     if request.method == "POST":
-        name = request.form.get('Name')
+        name = request.form.get('name')
         name_list.append(name)
-        return render_template("index.html", teacher=teacher, students_list=students_list, name=name, name_list=name_list)
+        lista = remove_duplicates(name_list)
+        return render_template("index.html", teacher=teacher, students_list=students_list, name=name, name_list=lista)
 
-@app.route("/plano-de-ensino")
+@app.route("/plan")
 def teaching_plan():
     return render_template("plan.html")
 
-@app.route("/initial-template")
+@app.route("/hello")
 def initial_template():
     return "<h1>Hello, World!</h1>"
 
+@app.route("/calculator", methods=["GET", "POST"])
+def simple_calculator():
+    if request.method == "GET":
+        return render_template("calculator.html")
+    if request.method == "POST":
+        expression = request.form.get('expression')
+        expression = expression.replace('÷','/').replace('𝗑', '*').replace(',', '.')
+        try:
+            result = eval(expression)
+            binnary = bin(int(result)).removeprefix('0b')
+            mensage = None
+        except:
+            mensage = 'Verifique se digitou corretamente'
+            result = binnary = None
+        return render_template("calculator.html", result=result, binnary=binnary, mensage=mensage)
--- static/js/calculadora.js
+++
@@ -1,9 +0,0 @@
-function insert(text) {
-  input = document.getElementById('expression');
-  old_value = input.value;
-  input.value = old_value + text;
-}
-
-function clean() {
-  document.getElementById('expression').value = '';
-}
